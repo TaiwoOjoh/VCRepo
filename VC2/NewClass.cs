0c3fa59c544e2102d32c8389fca42dbbc57a1171
Taiwo Ojoh <t.ojoh@outlook.com>
2019-01-03 11:31:57 +0100

    first commit

---
+++ VC2/NewClass.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace VC2
+{
+    public class NewClass
+    {
+        public NewClass()
+        {
+        }
+    }
+}
---
+++ VC2/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace VC2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter first number");
+            int num1 = Int32.Parse(Console.ReadLine());
+            Console.WriteLine("Enter second number");
+            int num2 = Int32.Parse(Console.ReadLine());
+            int result = num1 + num2;
+            Console.WriteLine(result);
+        }
+    }
+}
